4d01270fe3dc60e566fc77d4bd3f36f679689aea
Gailbear <gterman@gmail.com>
2024-09-13 09:08:18 -0400

    Implement loading from url params

--- src/main.ts
+++ src/main.ts
@@ -1,44 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import 'zone.js';
-import { ScheduleComponent } from './app/schedule/schedule.component';
-import { ParserService } from './app/services/parser.service';
-import { CommonModule } from '@angular/common';
-import { ScheduleService } from './app/services/schedule.service';
-import { ConfigComponent } from './app/config/config.component';
+import { RouterModule, provideRouter, withComponentInputBinding } from '@angular/router';
+import { AppComponent } from './app/app.component';
+
+const routes = [
+  {
+    path: '',
+    component: AppComponent
+  }
+]
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ScheduleComponent, CommonModule, ConfigComponent],
-  providers: [ParserService],
+  imports: [RouterModule],
   template: `
-    <div class="styling">
-      <h2 class="dates">
-        <span>{{ schedule.date() | date}}</span>
-        <span>{{ schedule.hdate() }}</span>
-      </h2>
-    <app-schedule [schedule]="schedule.items()"></app-schedule>
-</div>
-<app-config></app-config>
-  `,
-  styles: `
-.styling {
-  max-width: 600px;
-  margin: 0 auto;
-}
-
-.dates {
-  display: flex;
-  justify-content: space-between;
-}
+    <router-outlet></router-outlet>
   `,
 })
-export class App implements OnInit {
-  constructor(public schedule: ScheduleService) {}
-  ngOnInit() {
-    this.schedule.init();
-  }
+export class App {
 }
 
-bootstrapApplication(App);
+bootstrapApplication(App,{ providers: [
+  provideRouter(routes, withComponentInputBinding())
+]});
